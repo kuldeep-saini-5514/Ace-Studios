0e9e167b52d580b8cf78c2a309cd060baa5e8c68
kuldeep <kuldeepsaini5514@gmail.com>
2024-02-12 12:58:38 +0530

    new changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -118,66 +118,66 @@ const Main = () => {
               solutions through a collaborative process, unlocking success for
               every project.
             </p>
-           <div className="flex flex-col gap-5 mt-10">
-           <div className="text-black">
-              <div className="flex gap-4">
-              <div className="flex justify-center items-center bg-skin h-10 w-10 rounded-xl">
-              <i class="fa-solid fa-rocket text-xl text-brown"></i>
-              </div>
-              <p className="text-black text-2xl font-semibold">Discovery</p>
-              </div>
-              <p className="pl-14 pt-2 text-gray">
+            <div className="flex flex-col gap-5 mt-10">
+              <div className="text-black">
+                <div className="flex gap-4">
+                  <div className="flex justify-center items-center bg-skin h-10 w-10 rounded-xl">
+                    <i class="fa-solid fa-rocket text-xl text-brown"></i>
+                  </div>
+                  <p className="text-black text-2xl font-semibold">Discovery</p>
+                </div>
+                <p className="pl-14 pt-2 text-gray">
                   {" "}
                   Dive deep into your vision: We actively listen and understand
                   your unique goals, challenges, and audience. Strategic
                   planning ensures measurable success with open communication
                   throughout.{" "}
                 </p>
-            </div>
-            <div className="text-black">
-              <div className="flex gap-4">
-              <div className="flex justify-center items-center bg-lightblue h-10 w-10 rounded-xl">
-              <i class="fa-solid fa-lightbulb text-xl text-DarkBlue"></i>
               </div>
-              <p className="text-black text-2xl font-semibold">Plan</p>
-              </div>
-              <p className="pl-14 pt-2 text-gray">
+              <div className="text-black">
+                <div className="flex gap-4">
+                  <div className="flex justify-center items-center bg-lightblue h-10 w-10 rounded-xl">
+                    <i class="fa-solid fa-lightbulb text-xl text-DarkBlue"></i>
+                  </div>
+                  <p className="text-black text-2xl font-semibold">Plan</p>
+                </div>
+                <p className="pl-14 pt-2 text-gray">
                   {" "}
                   We craft a clear roadmap together, leveraging expertise to
                   guide your project. Flexibility is key; we adapt to your
                   preferences, be it agile sprints or structured approaches.{" "}
                 </p>
-            </div>
-            <div className="text-black">
-              <div className="flex gap-4">
-              <div className="flex justify-center items-center bg-lightgreen h-10 w-10 rounded-xl">
-              <i class="fa-solid fa-rocket text-xl text-green"></i>
               </div>
-              <p className="text-black text-2xl font-semibold">Execute</p>
-              </div>
-              <p className="pl-14 pt-2 text-gray">
+              <div className="text-black">
+                <div className="flex gap-4">
+                  <div className="flex justify-center items-center bg-lightgreen h-10 w-10 rounded-xl">
+                    <i class="fa-solid fa-rocket text-xl text-green"></i>
+                  </div>
+                  <p className="text-black text-2xl font-semibold">Execute</p>
+                </div>
+                <p className="pl-14 pt-2 text-gray">
                   {" "}
                   Seamless teamwork ensures efficient delivery with rigorous
                   quality control. Regular feedback keeps you informed and
                   involved, shaping the project's outcome every step of the way.{" "}
                 </p>
-            </div>
-            <div className="text-black">
-              <div className="flex gap-4">
-              <div className="flex justify-center items-center bg-lightyellow h-10 w-10 rounded-xl">
-              <i class="fa-solid fa-rocket text-lg text-DarkYellow"></i>
               </div>
-              <p className="text-black text-2xl font-semibold">Deliver</p>
-              </div>
-              <p className="pl-14 pt-2 text-gray">
+              <div className="text-black">
+                <div className="flex gap-4">
+                  <div className="flex justify-center items-center bg-lightyellow h-10 w-10 rounded-xl">
+                    <i class="fa-solid fa-rocket text-lg text-DarkYellow"></i>
+                  </div>
+                  <p className="text-black text-2xl font-semibold">Deliver</p>
+                </div>
+                <p className="pl-14 pt-2 text-gray">
                   {" "}
                   Exceeding expectations, we deliver a product that aligns
                   perfectly with your goals. Beyond launch, ongoing support
                   optimizes long-term success, building trust and value for
                   future collaborations.{" "}
                 </p>
+              </div>
             </div>
-           </div>
           </div>
         </div>
       </div>
@@ -191,11 +191,13 @@ const Main = () => {
         </div>
         {/* right box */}
         <div>
-          <p className="pl-10 ml-4 h-[10rem] text-lg border-l-[3px] border-Gray  text-gray">We don't just build solutions, we tailor them to your unique needs.
-          From data-driven insights to agile collaboration, we work closely with
-          you to deliver cutting-edge solutions that drive real results. Backed
-          by a proven track record of success, we're your trusted partner in
-          achieving your digital goals.</p>
+          <p className="pl-10 ml-4 h-[10rem] text-lg border-l-[3px] border-Gray  text-gray">
+            We don't just build solutions, we tailor them to your unique needs.
+            From data-driven insights to agile collaboration, we work closely
+            with you to deliver cutting-edge solutions that drive real results.
+            Backed by a proven track record of success, we're your trusted
+            partner in achieving your digital goals.
+          </p>
         </div>
       </div>
 
@@ -226,18 +228,52 @@ const Main = () => {
       </div>
 
       {/* TESTIMONIALS */}
-      <div>
+      <div className="flex mt-20 justify-center items-center">
         {/* left */}
-        <div>
-          <img
-            className=""
-            src={require("./Images/Huawei-logo-A8C7CBCAA8-seeklogo.com.png")}
-            alt="logo"
-          />
-        </div>
-        <div></div>
+        <div className="h-[30rem] w-[160rem] m-5">
+          <div className="absolute left-[14rem] w-[8rem] h-[8rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-1.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[25rem] top-[221rem] w-[6rem] h-[6rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-2.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute top-[224rem] w-[8rem] h-[8rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-3.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[12rem] top-[231rem] w-[7rem] h-[7rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-4.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[22rem] top-[230rem] w-[7rem] h-[7rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-5.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[33rem] top-[226rem] w-[6rem] h-[6rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-6.jpeg')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[2rem] top-[236rem] w-[8rem] h-[8rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-8.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[16rem] top-[240rem] w-[8rem] h-[8rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-3.png')})`,backgroundSize:"cover"}}></div>
+          <div className="absolute left-[30rem] top-[237rem] w-[8rem] h-[8rem] rounded-xl"  style={{backgroundImage: `url(${require('./Images/TS-9.png')})`,backgroundSize:"cover"}}></div>
       </div>
-
+        {/* right */}
+        <div>
+          <p className="font-semibold text-[2rem] leading-10 w-[25rem]">Meet Client Satisfaction After Working With Us</p>
+          <p className="font-semibold m-0 p-0">Doesn't feel like an agency</p>
+          {/* icons */}
+          <div className="text-yellow mb-2 mt-2">
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          <i class="fa-solid fa-star"></i>
+          </div>
+          {/* icons */}
+          <p className="text-gray">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            doloribus voluptatibus minus tempore corporis, sapiente culpa quos
+            temporibus expedita sequi omnis commodi! Eligendi, beatae animi ut
+            praesentium possimus natus totam! Voluptates quidem earum nemo
+            harum, pariatur soluta cumque eaque molestias aut facilis
+            
+          </p>
+          {/* User */}
+          <div className="flex">
+            {/* left */}
+            <div className="w-[4rem] h-[4rem] rounded-xl"style={{backgroundImage: `url(${require("./Images/TS-logo.png")})`,backgroundSize: "100% 110%",}}></div>
+            {/* right */}
+            <div className="flex flex-col ml-5">
+              <p className="p-0 font-semibold m-0">Gwen Stacy</p>
+              <p className="p-0 m-0 text-gray mt-2">Cloudexter</p>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* //Contact  */}
       <div>
         <div className="flex-col text-center">
@@ -245,22 +281,22 @@ const Main = () => {
           <p>Felis nunc, aliquet quam dictum senectus nunc.</p>
         </div>
 
-        <div className="flex m-10 bg-white shadow-2xl rounded-3xl">
+        <div className="flex m-10 text-white bg-white shadow-2xl rounded-3xl">
           {/* //left side */}
           <div className="bg-purple text-white rounded-xl px-8 py-6 w-[25rem] h-[30rem]">
             <div>
-              <h5 className="text-xl font-semibold mb-2">
+              <p className="text-2xl font-semibold mb-2">
                 {" "}
                 Contact Information
-              </h5>
-              <p className="mb-10">
+              </p>
+              <p className="mb-10 text-Gray">
                 {" "}
                 If you like to work with us then drop us a message
               </p>
             </div>
             <div className="bg-white h-10 w-10 flex px-3 py-1.5  rounded-full mb-6">
               <i class="fa-solid fa-mobile mr-10 text-purple"></i>
-              <span>+91 9720623941</span>
+              <span>+919720623941</span>
             </div>
             <div className="bg-white h-10 w-10 flex px-3 py-1.5  rounded-full mb-6 ">
               <div className="flex">
@@ -277,46 +313,45 @@ const Main = () => {
               <span>India</span>
             </div>
           </div>
-
-          {/* //right side  */}
+          {/* right side */}
           <div className="">
-            <form class="flex flex-wrap ml-10 w-[40rem]">
-              <div>
-                <h5>Full Name</h5>
-                <input type="text" />
-              </div>
-              <div>
-                <h5>Email</h5>
-                <input type="email" />
-              </div>
-              <div>
-                <h5>Phone</h5>
-                <input type="email" />
-              </div>
-              <div>
-                <h5>Email</h5>
-                <input type="email" />
-              </div>
-              <input
-                placeholder="Rating (1-5)"
-                class="bg-gray-700 text-black border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-                type="number"
-              />
-              <textarea
-                placeholder="Feedback"
-                class="bg-gray-700 text-black border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-purple focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
-                name="feedback"
-              />
+          <form class="flex flex-wrap ml-10 w-[40rem]">
+            <div>
+              <h5>Full Name</h5>
+              <input type="text" />
+            </div>
+            <div>
+              <h5>Email</h5>
+              <input type="email" />
+            </div>
+            <div>
+              <h5>Phone</h5>
+              <input type="email" />
+            </div>
+            <div>
+              <h5>Email</h5>
+              <input type="email" />
+            </div>
+            <input
+              placeholder="Rating (1-5)"
+              class="bg-gray-700 text-black border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+              type="number"
+            />
+            <textarea
+              placeholder="Feedback"
+              class="bg-gray-700 text-black border-0 rounded-md p-2 mb-4 focus:bg-gray-600 focus:outline-purple focus:ring-1 focus:ring-blue-500 transition ease-in-out duration-150"
+              name="feedback"
+            />
 
-              <button
-                class="bg-gradient-to-r from-blue-500 to-blue-500 text-blackfont-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
-                type="submit"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
+            <button
+              class="bg-gradient-to-r from-blue-500 to-blue-500 text-blackfont-bold py-2 px-4 rounded-md mt-4 hover:bg-indigo-600 hover:to-blue-600 transition ease-in-out duration-150"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
         </div>
+        </div>        
       </div>
     </>
   );
